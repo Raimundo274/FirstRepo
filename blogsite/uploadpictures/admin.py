87f33818e3df83e4750b6c8cc28ac87b26d0179d
Raimundo <Ray@ftwgroup.com>
2012-06-23 20:37:56 -0700

    Test

---
+++ blogsite/uploadpictures/admin.py
@@ -0,0 +1,5 @@
+from uploadpictures.models import Picture
+from django.contrib import admin
+
+
+admin.site.register(Picture)
--- blogsite/uploadpictures/models.py
+++ blogsite/uploadpictures/models.py
@@ -4,9 +4,9 @@ from django.conf import settings
 
 #class Filefield(upload_to = os.path.join(PROJECT_ROOT,'media')[, max_length=1000, **options])
 
-class Picture(models.Model):
+class Picture(models.Model, ):
 	caption = models.CharField(blank=True, max_length= 50)
 	image = models.ImageField(upload_to='uploads/')
 	pub_date = models.DateTimeField('Date uploaded')
-
+	
 # Create your models here.
